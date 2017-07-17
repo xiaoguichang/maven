29cbb1d41bdee91871dff7ff4d8b9e5c0a4bd78b
xiaoguichang <guichangxiao@163.com>
2017-07-17 21:33:57 +0800

    add maven-redis

---
+++ maven-redis/src/main/java/com/xiaogch/maven/redis/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.xiaogch.maven.redis;
+
+/**
+ * Created by Administrator on 2017/7/17 0017.
+ */
+public class RedisConfig {
+
+}
---
+++ maven-redis/src/main/java/com/xiaogch/maven/redis/common/Jedis.java
@@ -0,0 +1,8 @@
+package com.xiaogch.maven.redis.common;
+
+/**
+ * Created by Administrator on 2017/7/17 0017.
+ */
+public class Jedis {
+
+}
