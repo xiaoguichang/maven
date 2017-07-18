1065eccbf3d8848397cd58ba7fa389a1ecd76000
xiaoguichang <guichangxiao@163.com>
2017-07-18 22:25:20 +0800

    add HttpRequestUtil、EmailUtil、HttpRequestException

---
+++ maven-common/src/main/java/com/xiaogch/maven/common/util/EmailUtil.java
@@ -0,0 +1,79 @@
+package com.xiaogch.maven.common.util;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+
+public class EmailUtil {
+	
+	/**
+	 * 发送邮件 
+	 * @param title 邮件标题
+	 * @param content 邮件内容
+	 * @param propties 邮件发送配置信息<br>mail.smtp.host, mail.smtp.from , mail.smtp.user , mail.smtp.password
+	 * @param addresses 收件人列表
+	 * @throws MessagingException
+	 */
+	public static void sendTextEmail(String title , String content , Properties propties , String...addresses) throws Exception {
+		Assert.isTrue(propties != null && !propties.isEmpty() , "email properties can't be null or empty");
+		Assert.isTrue(StringUtils.hasText(title), "emial title can't be null or empty");
+		Assert.isTrue(StringUtils.hasText(content), "emial content can't be null or empty");
+		Assert.isTrue((addresses != null && addresses.length != 0), "emial address can't be null or empty");
+		parameterCheck(propties);
+		Session session = getSession(propties);
+		MimeMessage message = new MimeMessage(session);
+		message.setText(content);
+		message.setSubject(title, "utf-8");
+//		设置抄送人员
+//		message.setRecipients(RecipientType.BCC, addresses);
+//		设置暗抄送人员
+//		message.setRecipient(RecipientType.CC, address);
+		Address[] eamilAddress = new InternetAddress[addresses.length];
+		int index = 0;
+		for (String address : addresses) {
+			eamilAddress[index++] = new InternetAddress(address);
+		}
+//		设置收件人
+		message.setRecipients(RecipientType.TO, eamilAddress);
+		Transport.send(message);
+	}
+	
+	private static Session getSession(final Properties propties) {
+		Session session;
+		if (!propties.containsKey("mail.smtp.auth")) {
+			propties.setProperty("mail.smtp.auth", "true");
+		}
+		if ("true".equals(propties.get("mail.smtp.auth").toString())) {
+			session = Session.getDefaultInstance(propties , new Authenticator() {
+				@Override
+				public PasswordAuthentication getPasswordAuthentication() {
+					String userName = propties.get("mail.smtp.user").toString();
+					String password = propties.get("mail.smtp.password").toString();
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+		} else {
+			session = Session.getDefaultInstance(propties);
+		}
+		return session;
+	}
+
+	private static void parameterCheck(Properties propties) {
+		Assert.notNull(propties.get("mail.smtp.host") , "mail.smtp.host property can't be null");
+		Assert.notNull(propties.get("mail.smtp.from") , "mail.smtp.from property can't be null");
+		Assert.notNull(propties.get("mail.smtp.user") , "mail.smtp.user property can't be null");
+		Assert.notNull(propties.get("mail.smtp.password") , "mail.smtp.password property can't be null");
+	}	
+}
---
+++ maven-common/src/main/java/com/xiaogch/maven/common/util/HttpRequestUtil.java
@@ -0,0 +1,80 @@
+package com.xiaogch.maven.common.util;
+
+import com.xiaogch.maven.common.exception.HttpRequestException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.*;
+
+public class HttpRequestUtil {
+	
+	/** http 请求超市  */
+	private final static int timeout = 60000; 
+	
+	/**
+	 * 发送post请求，若请求成功返回响应信息，否则抛出HttpRequestException异常
+	 * @param url 请求地址 
+	 * @param requestParameter 请求参数
+	 * @return
+	 * @throws HttpRequestException 请求异常
+	 */
+	public static String doPostAndReturnString(String url , Map<String , String> requestParameter) throws HttpRequestException {
+		HttpPost httpPost = new HttpPost(url);
+		RequestConfig config = RequestConfig.custom()
+				.setConnectTimeout(timeout)
+				.setConnectTimeout(timeout)
+				.setSocketTimeout(timeout)
+				.build();
+		httpPost.setConfig(config);
+		
+		HttpClient client = HttpClients.createDefault();
+		HttpResponse response;
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		if (requestParameter != null && !requestParameter.isEmpty()) {
+			Set<String> keys = requestParameter.keySet();
+			for (String key : keys) {
+				nameValuePairs.add(new BasicNameValuePair(key, requestParameter.get(key)));
+			}
+		}
+	
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs , "utf-8"));
+			response = client.execute(httpPost);
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+				return EntityUtils.toString(response.getEntity());
+			} else {
+				StringBuilder sb = new StringBuilder(response.getStatusLine().toString());
+				sb.append("\n");
+				sb.append(EntityUtils.toString(response.getEntity()));
+				throw new HttpRequestException(sb.toString());
+			}
+		} catch (ClientProtocolException e) {
+			throw new HttpRequestException(e.getMessage() , e);
+		} catch (IOException e) {
+			throw new HttpRequestException(e.getMessage() , e);
+		}
+	}
+	
+	public static void main(String[] argvs) {
+		try {
+			Map<String , String> requestParameter = new HashMap<String , String>();
+			requestParameter.put("employee_263", "xiaoguichang");
+			requestParameter.put("app", "dailyReport");
+			requestParameter.put("app_token", "imwCAt29zoyB6AYwGxhOfg");
+			String result = doPostAndReturnString("http://localhost:8080/dailyreport-war/j_spring_security_check1?j_username1=xiaoguichang&j_password=1235", requestParameter);
+			System.err.println(result);
+		} catch (HttpRequestException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
