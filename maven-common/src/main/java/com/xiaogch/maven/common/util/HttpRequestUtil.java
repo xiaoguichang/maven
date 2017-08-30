7479f5c89ede4e7326caa1252304b013e2217ded
xiaoguichang_shangde <guichangxiao@163.com>
2017-08-30 15:38:24 +0800

    modefy httpRequestUtil add get method

--- maven-common/src/main/java/com/xiaogch/maven/common/util/HttpRequestUtil.java
+++ maven-common/src/main/java/com/xiaogch/maven/common/util/HttpRequestUtil.java
@@ -11,25 +11,62 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.*;
 
 public class HttpRequestUtil {
 	
-	/** http 请求timeout  */
-	private final static int timeout = 60000; 
+	/** http 请求 connectTimeout  */
+	private final static int CONNECT_TIMEOU = 60000;
+
+	/** http 请求 soketTimeout  */
+	private final static int SOCKET_TIMEOU = 60000;
+
+	/**
+	 * 发送post请求，若请求成功返回响应信息，否则抛出HttpRequestException异常，键值对的方式
+	 * @param url 请求地址
+	 * @param requestParameter 请求参数
+	 * @param connectTimeout 连接超时
+	 * @param socketTimeOut 读取超时
+	 * @return http response 返回的数据
+	 * @throws HttpRequestException 请求异常
+	 */
+	public static String doPostAndReturnString(String url , String requestParameter , int connectTimeout , int socketTimeOut) throws HttpRequestException {
+		HttpPost httpPost = new HttpPost(url);
+		RequestConfig config = getRequestConfig(connectTimeout , socketTimeOut);
+		httpPost.setConfig(config);
+		try {
+			httpPost.setEntity(new StringEntity(requestParameter , "utf-8"));
+		} catch (UnsupportedCharsetException e) {
+			throw new HttpRequestException(e.getMessage() , e);
+		}
+		return sendReqeust(httpPost);
+	}
+
+	/**
+	 * 发送post请求，若请求成功返回响应信息，否则抛出HttpRequestException异常，键值对的方式
+	 * @param url 请求地址
+	 * @param requestParameter 请求参数
+	 * @return http response 返回的数据
+	 * @throws HttpRequestException 请求异常
+	 */
+	public static String doPostAndReturnString(String url , String requestParameter) throws HttpRequestException {
+		return doPostAndReturnString(url , requestParameter , CONNECT_TIMEOU , SOCKET_TIMEOU);
+	}
 	
 	/**
 	 * 发送post请求，若请求成功返回响应信息，否则抛出HttpRequestException异常，键值对的方式
 	 * @param url 请求地址 
 	 * @param requestParameter 请求参数
-	 * @param connectTimeout
-	 * @param socketTimeOut
+	 * @param connectTimeout 连接超时
+	 * @param socketTimeOut 读取超时
 	 * @return http response 返回的数据
 	 * @throws HttpRequestException 请求异常
 	 */
@@ -54,13 +91,30 @@ public class HttpRequestUtil {
 	 * @throws HttpRequestException 请求异常
 	 */
 	public static String doPostAndReturnString(String url , Map<String , String> requestParameter) throws HttpRequestException {
-		return doPostAndReturnString(url , requestParameter , timeout , timeout);
+		return doPostAndReturnString(url , requestParameter , CONNECT_TIMEOU , SOCKET_TIMEOU);
 	}
 
 
-	public static String doGetAndReturnString(String url , int timeOut) throws HttpRequestException {
+	/**
+	 * 发送get请求，若请求成功返回响应信息，否则抛出HttpRequestException异常
+	 * @param url 请求地址
+	 * @return http response 返回的数据
+	 * @throws HttpRequestException 请求异常
+	 */
+	public static String doGetAndReturnString(String url) throws HttpRequestException {
+		return doGetAndReturnString(url , CONNECT_TIMEOU , SOCKET_TIMEOU);
+	}
+	/**
+	 * 发送get请求，若请求成功返回响应信息，否则抛出HttpRequestException异常
+	 * @param url 请求地址
+	 * @param connectTimeout 连接超时
+	 * @param socketTimeOut 读取超时
+	 * @return http response 返回的数据
+	 * @throws HttpRequestException 请求异常
+	 */
+	public static String doGetAndReturnString(String url , int connectTimeout , int socketTimeOut) throws HttpRequestException {
 		HttpGet httpGet = new HttpGet(url);
-		RequestConfig config = getRequestConfig(timeOut , timeOut);
+		RequestConfig config = getRequestConfig(connectTimeout , socketTimeOut);
 		httpGet.setConfig(config);
 		return sendReqeust(httpGet);
 	}
@@ -103,24 +157,6 @@ public class HttpRequestUtil {
 				.build();
 		return config;
 	}
-
-
-
-
-
-
-//	public static void main(String[] argvs) {
-//		try {
-//			Map<String , String> requestParameter = new HashMap<String , String>();
-//			requestParameter.put("employee_263", "xiaoguichang");
-//			requestParameter.put("app", "dailyReport");
-//			requestParameter.put("app_token", "imwCAt29zoyB6AYwGxhOfg");
-//			String result = doPostAndReturnString("http://localhost:8080/dailyreport-war/j_spring_security_check1?j_username1=xiaoguichang&j_password=1235", requestParameter);
-//			System.err.println(result);
-//		} catch (HttpRequestException e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
 
 
 }
